82ad3d47a494922744aea8a0864ebfbbd8ce0b7b
cshooon <0015chseang@gmail.com>
2022-10-05 11:14:00 +0900

    CBV로 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,35 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts
-        }
-    )
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-    return render(
-        request,
-        'blog/single_page.html',
-        {
-            'post': post,
-        }
-    )
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+
+class PostDetail(DetailView):
+     model = Post
+
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts
+#         }
+#     )
+
+
+# def single_post_page(request, pk):
+#     post = Post.objects.get(pk=pk)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
 
